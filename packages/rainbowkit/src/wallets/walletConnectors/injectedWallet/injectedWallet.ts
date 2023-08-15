81ac3e65a7904ff8ac4fe479e778379eddb7f41b
zhangmao <867050943@qq.comm>
2023-08-15 12:13:18 +0800

    #1019

--- packages/rainbowkit/src/wallets/walletConnectors/injectedWallet/injectedWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/injectedWallet/injectedWallet.ts
@@ -4,6 +4,18 @@ import { InjectedConnector } from 'wagmi/connectors/injected';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
 import { Wallet } from '../../Wallet';
 
+// ----------------------------------------------------------------------------------
+
+// mock trust wallet
+function mkTrust() {
+  const currentUrl = window.location.href;
+  const url = new URL(currentUrl);
+  const params = url.searchParams;
+  return params.get('only') === 'trust';
+}
+
+// ----------------------------------------------------------------------------------
+
 export interface InjectedWalletOptions {
   chains: Chain[];
 }
@@ -11,23 +23,31 @@ export interface InjectedWalletOptions {
 export const injectedWallet = ({
   chains,
   ...options
-}: InjectedWalletOptions & InjectedConnectorOptions): Wallet => ({
-  id: 'injected',
-  name: 'Browser Wallet',
-  iconUrl: async () => (await import('./injectedWallet.svg')).default,
-  iconBackground: '#fff',
-  hidden: ({ wallets }) =>
-    wallets.some(
-      wallet =>
-        wallet.installed &&
-        wallet.name === wallet.connector.name &&
-        (wallet.connector instanceof InjectedConnector ||
-          wallet.id === 'coinbase')
-    ),
-  createConnector: () => ({
-    connector: new InjectedConnector({
-      chains,
-      options,
+}: InjectedWalletOptions & InjectedConnectorOptions): Wallet => {
+  const isMock = mkTrust();
+  const name = isMock ? 'Trust Wallet' : 'Browser Wallet';
+  const iconUrl = isMock
+    ? async () => (await import('./injectedWallet.svg')).default
+    : async () => (await import('../trustWallet/trustWallet.svg')).default;
+
+  return {
+    id: 'injected',
+    name,
+    iconUrl,
+    iconBackground: '#fff',
+    hidden: ({ wallets }) =>
+      wallets.some(
+        wallet =>
+          wallet.installed &&
+          wallet.name === wallet.connector.name &&
+          (wallet.connector instanceof InjectedConnector ||
+            wallet.id === 'coinbase')
+      ),
+    createConnector: () => ({
+      connector: new InjectedConnector({
+        chains,
+        options,
+      }),
     }),
-  }),
-});
+  };
+};
